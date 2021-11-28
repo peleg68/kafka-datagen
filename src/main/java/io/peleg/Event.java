d776fd4f5d9399040d6b0aadfe919aab6f82c206
Peleg Tsadok <pelegtsa@gmail.com>
2021-11-28 18:21:28 +0200

    Single class imports

--- src/main/java/io/peleg/Event.java
+++ src/main/java/io/peleg/Event.java
@@ -1,6 +1,10 @@
 package io.peleg;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
